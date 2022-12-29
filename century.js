3d196c38834561f19796e0987603c23c702e6abe
Chase_Haynes <chase.haynez@gmail.com>
2022-12-29 12:02:47 -0500

    final
    math section done

---
+++ century.js
@@ -0,0 +1,70 @@
+// problem 1 Math.ceil() "Math.ceil(x) returns the value of x rounded up to its nearest integer"
+
+// initiate a function
+function mathCeil(ceil) {
+    // place your return value with "Math.ceil"
+    return Math.ceil(ceil / 100);
+}
+// define variable ceilResult
+let ceilResult = Math.ceil(1000 / 100);
+// run console
+console.log(ceilResult);
+
+
+
+// problem 2 Math.round() "Math.round(x) returns the nearest integer"
+
+// initiate function
+function mathRound(round) {
+    // place your return value with "Math.ceil"
+    return Math.round(round);
+}
+// define variable roundResult
+let roundResult = Math.round(7.5);
+ // run console
+console.log(roundResult);
+
+
+
+ //  problem 3 Math.floor() "Math.floor(x) returns the value of x rounded down to its nearest integer"
+
+// initate a function
+function mathFloor(floor) {
+    // place your return value with "Math.floor"
+    return Math.floor(floor);
+}
+// define variable floorResult
+let floorResult = Math.floor(5.5);
+// run console
+console.log(floorResult);
+
+
+
+// problem 4 Math.trunc() "Math.trunc(x) returns the integer part of x"
+
+// initiate a function
+function mathTrunc(trunc) {
+    // place your return value with "Math.trunc"
+    return Math.trunc(trunc);
+}
+// define variable truncResult
+let truncResult = Math.trunc(11.679384292898);
+// run console
+console.log(truncResult);
+
+
+
+// problem 5 Math.sign() "Math.sign(x) returns if x is negative, null or positive"
+
+// initiiate a fucntion 
+function mathSign(sign) {
+    // place your return value with "Math.sign"
+    return Math.sign(sign);
+}
+// define variable signResult
+let signResult = Math.sign(5)
+// let signResult = Math.sign(0)
+// let signResult = Math.sign(-5)
+
+// run console
+console.log(signResult);
